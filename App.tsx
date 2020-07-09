02af0b1372cba3c2936b96beaa56605ee0b12baa
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2020-07-09 20:46:54 -0300

    feat: add expo constants

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import expoConstants from 'expo-constants'
+
 import Routes from './src/routes'
 import { ThemeProvider } from './src/theme'
 
 const App:React.FC = () => {
+  console.log(expoConstants.manifest)
   return (
     <ThemeProvider>
       <Routes/>
